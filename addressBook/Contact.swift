2bb951250a107497ef40be353a3737e6e34c19eb
Selin Ersev <sersev96@gmail.com>
2018-11-27 18:40:18 +0300

    .

---
+++ addressBook/Contact.swift
@@ -0,0 +1,26 @@
+//
+//  File.swift
+//  addressBook
+//
+//  Created by Selin Ersev on 27.11.2018.
+//  Copyright © 2018 Selin Ersev. All rights reserved.
+//
+
+import Foundation
+
+class Contact{
+    
+    var name : String = ""
+    var surname : String = ""
+    var phone : String = ""
+    var company : String = ""
+    
+
+//    init(name:String, surname:String, phone:String, company:String ) {
+//        self.name = name
+//        self.surname = surname
+//        self.phone = phone
+//        self.company = company
+//    }
+
+}
